02417dd9a859548bb2a78742bfc665f02ace0858
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-01-05 14:14:18 +0300

    ...

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,11 +1,14 @@
 import { Component } from "@angular/core";
+import {Movies} from '../movie.datasource';
+
 
 @Component({
     selector: 'movies',
     templateUrl: 'movies.component.html',
-    styleUrls: ['movies.component.css']
 })
 
 export class MoviesComponent{
+    title: string = 'Filmler Sayfası';
+    movies= Movies;
 
 }

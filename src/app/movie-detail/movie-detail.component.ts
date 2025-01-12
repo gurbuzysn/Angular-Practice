e5342add51a3e34e3b790df224b4805344f57a8e
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-01-12 22:29:41 +0300

    Input Parameters

---
+++ src/app/movie-detail/movie-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Movie } from '../movie';
+
+@Component({
+  selector: 'movie-detail',
+  templateUrl: './movie-detail.component.html',
+  styleUrls: ['./movie-detail.component.css']
+})
+export class MovieDetailComponent {
+    selectedMovie:Movie;
+}

0b36cbe3d62e967f1c29fdb8ebdd676417086c48
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-01-12 22:22:58 +0300

    Directives

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -6,5 +6,5 @@ export const Movies: Movie[] = [
     {id: 2, name: "Movie 2"},
     {id: 3, name: "Movie 3"},
     {id: 4, name: "Movie 4"},
-    {id: 5, name: "Movie 55"},
+    {id: 5, name: "Movie 5"},
 ]

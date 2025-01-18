22abc88d5f5de64cbee7b0e46b1872f24019c35a
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-01-18 09:34:13 +0300

    logging service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { LoggingComponent } from './logging/logging.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     MovieComponent,
-    MovieDetailComponent
+    MovieDetailComponent,
+    LoggingComponent
   ],
   imports: [
     BrowserModule,

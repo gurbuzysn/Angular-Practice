adaa792e3c9a46d23f2f8cbae905a171b9c87345
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-01-20 23:15:47 +0300

    ..

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -7,7 +7,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
 
   createDb(){
-
+    const movies = [
+        {id: 1, name: "Movie 1", description:"Güzel Film", imageUrl:"1.jpg"},
+        {id: 2, name: "Movie 2", description:"Güzel Film 2", imageUrl:"2.jpg"},
+        {id: 3, name: "Movie 3", description:"Güzel Film 3", imageUrl:"3.jpg"},
+        {id: 4, name: "Movie 4", description:"Güzel Film 4", imageUrl:"4.jpg"},
+        {id: 5, name: "Movie 5", description:"Güzel Film 5", imageUrl:"5.jpg"},
+        {id: 6, name: "Movie 6", description:"Güzel Film 6", imageUrl:"6.jpg"},
+        {id: 7, name: "Movie 7", description:"Güzel Film 7", imageUrl:"7.jpg"},
+    ];
+    return {movies};
   }
   constructor() { }
 }
